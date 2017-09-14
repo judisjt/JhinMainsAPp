9a2e6bd47fb00105b521def5dcc5defcbb42342d
judisjt <31445081+judisjt@users.noreply.github.com>
2017-09-14 11:50:50 -0400

    second commit

--- src/app/fun-stuff/fun-stuff.component.ts
+++ src/app/fun-stuff/fun-stuff.component.ts
@@ -6,13 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fun-stuff.component.css']
 })
 export class FunStuffComponent implements OnInit {
-  value = '';
+  value = 'a';
+  value2 = 'a';
+  value3 = 'a';
+  value4 = 'a';
+  value5 = 'hello';
   firstAnswer = '';
+  secondAnswer = '';
+  thirdAnswer = '';
+  fourthAnswer = '';
   constructor() { }
 
   ngOnInit() {}
-  answer1(box: string){
+
+  answer1(box: string) {
     this.value = box;
-    this.firstAnswer = 'rapid fire';
+    this.firstAnswer = 'Rapid fire';
+  }
+  answer2(box2: string) {
+    this.value2 = box2;
+    this.secondAnswer = 'tawdry';
+  }
+  answer3(box3: string) {
+    this.value3 = box3;
+    this.thirdAnswer = 'perfection';
+  }
+  answer4(box4: string) {
+    this.value4 = box4;
+    this.fourthAnswer = 'shot';
   }
 }
